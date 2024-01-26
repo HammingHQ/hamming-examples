93085f77732537867bcf24801da0fc7c7d4736c6
Marius Buleandra <mbuleandra@gmail.com>
2024-01-25 17:08:32 -0800

    deps

--- ts/src/createSampleDataset/sample-data.ts
+++ ts/src/createSampleDataset/sample-data.ts
@@ -1,43 +1,44 @@
-import { DatasetItemValue } from 'hamming-sdk';
+import { DatasetItemValue } from "hamming-sdk";
+
 
 const sampleData: DatasetItemValue[] = [
   {
     input: {
-      query: 'Where or how do I send in my booking confirmations?',
+      query: "Where or how do I send in my booking confirmations?",
     },
     output: {
       response:
-        'When reserving outside the XYZ Portal, please ensure to email the confirmation to commissions@XYZ.com. Your email should come from your official XYZ address.',
+        "When reserving outside the XYZ Portal, please ensure to email the confirmation to commissions@XYZ.com. Your email should come from your official XYZ address.",
     },
     metadata: {},
   },
   {
     input: {
-      query: 'What are the uses of drones in agriculture?',
+      query: "What are the uses of drones in agriculture?",
     },
     output: {
       response:
-        'Drones in agriculture are used for crop monitoring, soil and field analysis, planting, crop spraying, irrigation, and crop health assessment.',
+        "Drones in agriculture are used for crop monitoring, soil and field analysis, planting, crop spraying, irrigation, and crop health assessment.",
     },
     metadata: {},
   },
   {
     input: {
-      query: 'What is the boiling point of water?',
+      query: "What is the boiling point of water?",
     },
     output: {
       response:
-        'The boiling point of water is 100 degrees Celsius at sea level.',
+        "The boiling point of water is 100 degrees Celsius at sea level.",
     },
     metadata: {},
   },
   {
     input: {
-      query: 'What are the main functions of the roots of a plant?',
+      query: "What are the main functions of the roots of a plant?",
     },
     output: {
       response:
-        'The main functions of plant roots are to absorb nutrients and water, anchor the plant in the soil, and store food and nutrients.',
+        "The main functions of plant roots are to absorb nutrients and water, anchor the plant in the soil, and store food and nutrients.",
     },
     metadata: {},
   },
